89ebdbb56fd44144af8c6fb90a6a7f179f93de84
faishal <faishalmanzar@gmail.com>
2026-02-08 06:06:37 +0530

    Stop tracking dist folder

--- dist/browser/constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEFAULT_OPENCLAW_BROWSER_COLOR = exports.DEFAULT_OPENCLAW_BROWSER_PROFILE_NAME = void 0;
-exports.DEFAULT_OPENCLAW_BROWSER_PROFILE_NAME = "default";
-exports.DEFAULT_OPENCLAW_BROWSER_COLOR = "blue";
--- dist/infra/errors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.formatErrorMessage = formatErrorMessage;
-function formatErrorMessage(err) {
-    if (err instanceof Error) {
-        return err.message;
-    }
-    return String(err);
-}
